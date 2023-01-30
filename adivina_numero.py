70df6ab162c02ef5ce0bb7c2a46a47327fc019c7
Jvaldez10 <javaldezh10@gmail.com>
2023-01-30 18:27:14 -0500

    juego hecho

--- adivina_numero.py
+++ adivina_numero.py
@@ -1,1 +1,26 @@
-print("Empesemos a jugar a divina el numero !!!.")
+import random
+# crea funcion
+
+
+def adivina_el_numero(x):
+    print("*****************************************")
+    print("Empesemos a jugar a divina el numero !!!.")
+    print("*****************************************")
+    print(" trata de vencer adivinamdo el numero que muestra en pantalla...")
+    numero_aleatorio = random.randint(1, x)  # numero aleatorio entre 1 y x
+    minumero = 0
+    while minumero != numero_aleatorio:
+        # usuario ingresa el numero
+        minumero = input(f"adivina un numero entre 1 y {x}: ")
+        minumero = int(minumero)
+        #
+        if minumero < numero_aleatorio:
+            print("Intenta otra vez, este numero es muy pequeño.")
+        elif minumero > numero_aleatorio:
+            print("intenta otra vez, este numero es muy grnade.")
+
+    print(
+        f"Felicitaciones adivinaste el numero {numero_aleatorio} correctamente !!!.")
+
+
+adivina_el_numero(10)
